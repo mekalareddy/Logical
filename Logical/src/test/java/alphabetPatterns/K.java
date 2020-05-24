cd2ff6d22059f517075e10188e3b071d8bc91cd2
MEKALA REDDY <govardhanreddy@gmail.com>
2020-05-24 14:52:46 +0530

    Added alphabetPatterns

---
+++ Logical/src/test/java/alphabetPatterns/K.java
@@ -0,0 +1,23 @@
+package alphabetPatterns;
+
+public class K {
+
+	public static void main(String[] args) {
+
+		int n=5;
+
+		for(int i=0; i<n; i++)
+		{
+			for(int j=n/2; j<n; j++)
+			{
+				if(j==n/2 || i==j || i+j==n-1)
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
